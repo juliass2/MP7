8f1d3073b093b9db2a25b9ce71b98da29e25c225
Julia Son <juliass2@illinois.edu>
2017-12-06 11:15:23 -0600

    Added a demo of java graphics

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
 
-public class Game {
+public class Game extends JPanel{
 	//INSTANCE VARIABLES
 	private boolean gameEnded;
 	private Room[] rooms;
 	private Player ourPlayer;
+	
+	int x = 0;
+    int y = 0;
+    private void moveBall() {
+        x = x + 1;
+        y = y + 1;
+    }
 	
 	public Game () {
 		//This game has 7 Riddles.
@@ -17,9 +30,22 @@ public class Game {
 		gameEnded = false;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x, y, 30, 30);
+    }
+	
+	public static void main(String[] args) throws InterruptedException {
+		JFrame myFrame = new JFrame("GETOUTTAHERE");
+		Game game = new Game();
+		myFrame.add(game);
+		myFrame.setSize(900,800);
+		myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+		
 	}
 
 }
