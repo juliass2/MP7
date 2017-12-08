0dbdf2a7b98567aa647c688a39458acac0a6a155
Hyunsoo Lee <Lee@wirelessprv-10-193-43-68.near.illinois.edu>
2017-12-08 00:12:28 -0600

    Modified Player movement

--- src/Player.java
+++ src/Player.java
@@ -6,41 +6,67 @@ public class Player {
 	 * Number of lives the player has throughout the game.
 	 */
 	private int numOfLives;
-	
+
 	/**
 	 * The different letters of CSROCKS the player has.
 	 */
 	private ArrayList<Item> items;
+
+	/**
+	 * Location of the player
+	 */
+	private int location;
+
+	/**
+	 * Movement
+	 */
+	public final String[] MOVEMENTS = { new String("forward"), new String("back") };
 	
-	//CONSTRUCTORS
+	/** total number of rooms */
+	public final int NUM_OF_ROOMS = 8;
+
+	// CONSTRUCTORS
 	/**
 	 * Constructing a basic player.
 	 */
 	public Player() {
 		numOfLives = 5;
 		items = new ArrayList<Item>();
+		location = 0;
 	}
-	
-	//GETTERS
+
+	// GETTERS
 	/**
 	 * Look at the remaining lives.
+	 * 
 	 * @return number of lives remaining.
 	 */
 	public int getNumOfLives() {
 		return numOfLives;
 	}
-	
+
 	/**
 	 * Getter method for items ArrayList.
+	 * 
 	 * @return items arrayList
 	 */
 	public ArrayList<Item> getItems() {
 		return items;
 	}
 	
-	//METHODS
+	/**
+	 * Getter method to return where the player is located
+	 * 
+	 * @return the room number
+	 */
+	public int getLocation() {
+		return location;
+	}
+
+	// METHODS
 	/**
 	 * Allows player to look at their current collection of CSROCKS.
+	 * 
 	 * @return the list of what hey currently have found
 	 */
 	public String lookItems() {
@@ -50,19 +76,37 @@ public class Player {
 		}
 		return itemsList;
 	}
-	
+
 	/**
 	 * Player's life count decreases when this method is called.
 	 */
 	public void loseLife() {
 		numOfLives--;
 	}
-	
+
 	/**
 	 * Allows the player to move forward or backward into another room.
-	 * @param direction whether the player wants to move forward or back.
+	 * 
+	 * @param direction
+	 *            whether the player wants to move forward or back.
 	 */
-	public void move(String direction) {
-		return;
+	public void move(String direction, int location) {
+		if (direction.equals(MOVEMENTS[1])) {
+			if (location == 0) {
+				System.out.print("INVALID MOVEMENT. YOU HAVE REACHED THE BEGINNING.");
+			} else {
+				if (Game.rooms.get(location).isUnlocked()) {
+					location += 1;
+				} else {
+					System.out.print("INVALID MOVEMENT. THE DOOR IS LOCKED.");
+				}
+			}
+		} else if (direction.equals(MOVEMENTS[0])) {
+			if (location == NUM_OF_ROOMS - 1) {
+				System.out.print("INVALID MOVEMENT. YOU HAVE REACHED THE END.");
+			} else {
+				location -= 1;
+			}
+		}
 	}
 }
